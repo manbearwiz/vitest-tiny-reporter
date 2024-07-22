c1e51aa5329d9297e70bbc05d44ab96b974bbdd6
Kevin Brey <manbearwiz@gmail.com>
2024-07-22 14:03:42 -0500

    fix(build): use vite to emit cjs and esm formats

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import type { UserConfig } from 'vitest/config';
 
 export default {
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: 'src/index.ts',
+      name: 'vitest-tiny-reporter',
+    },
+    rollupOptions: {
+      external: ['tinyrainbow'],
+      output: {
+        globals: {
+          tinyrainbow: 'tinyrainbow',
+        },
+      },
+    },
+    sourcemap: true,
+  },
   test: {
     environment: 'happy-dom',
     coverage: {
       reporter: ['cobertura', 'text', 'html', 'lcov'],
       thresholds: {
         statements: 93.75,
-        branches: 82.14,
+        branches: 82.75,
         functions: 100,
         lines: 93.75,
         autoUpdate: true,
